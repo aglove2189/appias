6e2eb8f4f3ddd6ed21497e44ae356722b269b1de
Glover <akglover@eprod.com>
2019-11-07 12:34:59 -0600

    bug fix in reduce_memory_usage

--- appias/util.py
+++ appias/util.py
@@ -12,17 +12,22 @@ def memory_usage(df):
     usage = df.memory_usage(deep=True).sum() / 1024 ** 2
     return '{:0.2f} MB'.format(usage)
 
-def reduce_memory_usage(df):
+
+def reduce_memory_usage(df, verbose=0):
     """ Reduces memory used by pd.DataFrame.
     Params:
         df: pd.DataFrame
     """
-    print('Before Reduction:', memory_usage(df))
+    if verbose > 0:
+        print('Before Reduction:', memory_usage(df))
 
-    df_float_cols = df.select_dtypes(include=['float']).columns
+    df_float_cols = df.select_dtypes(include='float').columns
     df[df_float_cols] = df[df_float_cols].apply(pd.to_numeric, downcast='float')
 
-    df_int_cols = df.select_dtypes(include=['int']).columns
+    df_int_cols = df.select_dtypes(include='int').columns
     df[df_int_cols] = df[df_int_cols].apply(pd.to_numeric, downcast='unsigned')
 
-    print('After Reduction:', memory_usage(df))
+    if verbose > 0:
+        print('After Reduction:', memory_usage(df))
+
+    return df
